190b798f5498a81dbf9c85774bdead03375bf967
Dmitriy Kholodov <dmitriy.kholodov@skuvault.com>
2023-06-05 14:25:16 +0300

    VT-6052: remove config version

--- src/ThreeDCartAccessTests/BaseRestApiTests.cs
+++ src/ThreeDCartAccessTests/BaseRestApiTests.cs
@@ -8,9 +8,6 @@ namespace ThreeDCartAccessTests
 		protected string Token { get; set; }
 		protected int TimeZone { get; set; }
 
-		protected RestThreeDCartConfigBase GetConfig( ThreeDCartConfigVersion configVersion ) =>
-			configVersion == ThreeDCartConfigVersion.V1
-				? (RestThreeDCartConfigBase)new RestThreeDCartConfigV1( this.StoreUrl, this.Token, this.TimeZone )
-				: new RestThreeDCartConfigV2( this.StoreUrl, this.Token, this.TimeZone );
+		protected RestThreeDCartConfigV2 GetConfig() => new RestThreeDCartConfigV2( this.StoreUrl, this.Token, this.TimeZone );
 	}
 }
