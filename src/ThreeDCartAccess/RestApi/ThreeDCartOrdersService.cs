963b03506dc508845039f531438dd8e95f16b1bb
maxim.saltanov <maxim.saltanov@skuvault.com>
2023-09-06 08:39:17 +0300

    GUARD-3085 removed ParallelProcessExtensions methods from ThreeDCart and pointed to Integrations.Core instead

--- src/ThreeDCartAccess/RestApi/ThreeDCartOrdersService.cs
+++ src/ThreeDCartAccess/RestApi/ThreeDCartOrdersService.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ThreeDCartAccess.Extensions;
+using SkuVault.Integrations.Core.Extensions;
 using SkuVault.Integrations.Core.Logging;
 using ThreeDCartAccess.Resilience;
 using ThreeDCartAccess.RestApi.Misc;
